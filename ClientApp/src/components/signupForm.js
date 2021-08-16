e43d7291baf8d11f6c349e3eb71e0999fcdafd48
VirtualVasquez <mvasq95@hotmail.com>
2021-08-16 19:21:36 -0400

    login page frontend base done

--- ClientApp/src/components/signupForm.js
+++ ClientApp/src/components/signupForm.js
@@ -1,30 +1,9 @@
 import React, { Component } from 'react';
 
-export class LoginForm extends Component {
+export class SignupForm extends Component {
 
   render () {
     return (
-      // <div className="jumbotron">
-        <div className="row">
-          <div className="col-md-6 offset-md-3">
-            <h2 className="text-center">Only U Can Choose To Text</h2>
-            <form id="signInForm">
-              <div className="mb-3">
-                <label for="username" class="form-label">Username</label>
-                <input type="username" class="form-control" id="username" aria-describedby="usernameHelp"></input>
-              </div>
-              <div className="mb-3">
-                <label for="password" class="form-label">Password</label>
-                <input type="password" class="form-control" id="password" aria-describedby="passwordHelp"></input>
-              </div>
-              <button type="submit" class="btn col-md-12 btn-primary">Submit</button>
-            </form>
-          </div>
-        </div>
-        /* <div className="row">
-          <div className="col-md-6 offset-md-3">            
-            <button class="btn btn-secondary col-md-6">Continue As Guest</button>
-            <button type="button" data-bs-toggle="modal" data-bs-target="#createAccountModal" class="btn btn-danger col-md-6">Create An Account</button>
             <div class="modal fade" id="createAccountModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
               <div class="modal-dialog">
                 <div class="modal-content">
@@ -51,9 +30,6 @@ export class LoginForm extends Component {
                 </div>
               </div>
             </div>
-          </div>
-        </div>
-      </div> */
     );
   }
 }
--- ClientApp/src/pages/loginPage/loginPage.js
+++ ClientApp/src/pages/loginPage/loginPage.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { LoginForm } from '../../components/loginForm.js';
-
+import { SignupForm } from '../../components/signupForm.js';
 
 export class LoginPage extends Component {
 
@@ -8,7 +8,16 @@ export class LoginPage extends Component {
     return (
       <div className="container">
         <h1 className="text-center">uText</h1>
-        <LoginForm />
+        <div className="jumbotron">
+          <LoginForm />
+          <div className="row">
+            <div className="col-md-6 offset-md-3">            
+              <button class="btn btn-secondary col-md-6">Continue As Guest</button>
+              <button type="button" data-bs-toggle="modal" data-bs-target="#createAccountModal" class="btn btn-danger col-md-6">Create An Account</button>
+            </div>
+          </div>
+          <SignupForm/>
+        </div>
       </div>
     );
   }
