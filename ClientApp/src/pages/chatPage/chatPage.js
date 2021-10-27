40827e8d88514659500f7c1c2c9455ffe9295842
VirtualVasquez <mvasq95@hotmail.com>
2021-10-27 07:59:07 -0400

    refactoring front-end

--- ClientApp/src/pages/chatPage/chatPage.js
+++ ClientApp/src/pages/chatPage/chatPage.js
@@ -5,62 +5,69 @@ export class ChatPage extends Component {
 
   render () {
     return (
-      <div className="container">
+        <div className="container" id="chat-page-container">
 
-        <div className="row">
-            <div class="col-6">
-                <h1>uText</h1>
-            </div>
-            <div class="col-6" id="logout-column">
-                <button className="btn btn-danger">Logout</button>
-            </div>
-        </div>
-        <div className="jumbotron" id="chat-jumbotron">
-
-            <div className="row" id="jumbotron-headers">
-                <div className="col-lg-2">
-                    <h3 className="text-center">Channels</h3>
+            <div className="row" id="nav-row">
+                <div class="col-6">
+                    <h1>uText</h1>
                 </div>
-                <div className="col-lg-8">
-                    <h2 className="text-center">CHANNEL NAME</h2>
-                </div>
-                <div className="col-lg-2">
-                    <h3 className="text-center">Active</h3>
+                <div class="col-6" id="logout-column">
+                    <button className="btn btn-danger">Logout</button>
                 </div>
             </div>
-            <div className="row" id="jumbotron-content">
-                <div className="col-lg-2">
-                    <button className="btn">Channel Name 1</button>
+
+            <div className="row" id="jumbotron-row">
+                <div className="jumbotron">
+                    <p>hello</p>
                 </div>
-                <div className="col-lg-8" id="chat-jumbotron-center-column">
+            </div>
 
+            {/* <div className="jumbotron" id="chat-jumbotron">
 
-                    <div className="container" id="chat-container">
-
-                        <div id="chat-history">
-                            <div className="author-and-text">
-                                <h6>Author</h6>
-                                <p>text</p>
-                            </div>
-                            <div className="author-and-text">
-                                <h6>Author</h6>
-                                <p>text</p>
-                            </div>
-
-                        </div>
-                        
-                        <form className="row" id="chat-form">
-                            <input type="message" className="form-control col-lg-10" placeholder="Write your message here"></input>
-                            <button type="submit" class="btn btn-primary col-lg-2">Send</button>
-                        </form>
-
+                <div className="row" id="jumbotron-headers">
+                    <div className="col-md-2">
+                        <p className="text-center">Channels</p>
+                    </div>
+                    <div className="col-md-8">
+                        <h2 className="text-center">CHANNEL NAME</h2>
+                    </div>
+                    <div className="col-md-2">
+                        <h3 className="text-center">Active</h3>
                     </div>
                 </div>
-                <div className="col-lg-2">
-                    <button className="btn">User1</button>
+                <div className="row" id="jumbotron-content">
+                    <div className="col-md-2">
+                        <button className="btn">Channel Name 1</button>
+                    </div>
+                    <div className="col-md-8" id="chat-jumbotron-center-column">
+
+
+                        <div className="container" id="chat-container">
+
+                            <div id="chat-history">
+                                <div className="author-and-text">
+                                    <h6>Author</h6>
+                                    <p>text</p>
+                                </div>
+                                <div className="author-and-text">
+                                    <h6>Author</h6>
+                                    <p>text</p>
+                                </div>
+
+                            </div>
+                            
+                            <form className="row" id="chat-form">
+                                <input type="message" className="form-control col-md-10" placeholder="Write your message here"></input>
+                                <button type="submit" class="btn btn-primary col-md-2">Send</button>
+                            </form>
+
+                        </div>
+                    </div>
+                    <div className="col-md-2">
+                        <button className="btn">User1</button>
+                    </div>
                 </div>
-            </div>
-        </div>
+            </div> */}
       </div>
     );
   }
