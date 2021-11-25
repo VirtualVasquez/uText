1aa2da3f7246f404941401882957097a4bb9cb25
VirtualVasquez <mvasq95@hotmail.com>
2021-11-24 22:11:24 -0500

    added react-router-dom

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -1,14 +1,28 @@
 import React, { Component } from 'react';
 import { LoginPage } from './pages/loginPage/loginPage.js';
 import { ChatPage } from './pages/chatPage/chatPage.js';
-
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 
 export default class App extends Component {
 
   render () {
     return (
-      // <LoginPage />
-      <ChatPage/>
+      <Router>
+        <div className="app">
+          <Switch>
+            <Route exact path="/">
+              <LoginPage />
+            </Route>
+            <Router path="/chat">
+              <ChatPage />
+            </Router>
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
